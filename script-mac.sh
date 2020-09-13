88e76fdd320520b221516e579db7864532f5f549
kotaKat <35742614+dakotaKat@users.noreply.github.com>
2020-09-13 18:21:41 -0400

    Whoops, let's make vncuser an admin, not a user

--- script-mac.sh
+++ script-mac.sh
@@ -6,7 +6,7 @@ sudo dscl . -create /Users/vncuser
 sudo dscl . -create /Users/vncuser UserShell /bin/bash
 sudo dscl . -create /Users/vncuser RealName "VNC User"
 sudo dscl . -create /Users/vncuser UniqueID 1001
-sudo dscl . -create /Users/vncuser PrimaryGroupID 20
+sudo dscl . -create /Users/vncuser PrimaryGroupID 80
 sudo dscl . -create /Users/vncuser NFSHomeDirectory /Users/vncuser
 sudo dscl . -passwd /Users/vncuser mypassword
 sudo dscl . -passwd /Users/vncuser mypassword
