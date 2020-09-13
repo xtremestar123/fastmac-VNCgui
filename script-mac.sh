75ce85bde25f61c2e2db8a29417df935617a8e4d
kotaKat <35742614+dakotaKat@users.noreply.github.com>
2020-09-13 16:46:09 -0400

    Update script-mac.sh

--- script-mac.sh
+++ script-mac.sh
@@ -15,11 +15,9 @@ sudo dscl . -append /Groups/admin GroupMembership vncuser
 sudo /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -configure -allowAccessFor -allUsers -privs -all
 sudo /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -configure -clientopts -setvnclegacy -vnclegacy yes 
 
-#vnc password set workaround?
-sudo /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -configure -clientopts -setvncpw -vncpw hello
 #VNC password - http://hints.macworld.com/article.php?story=20071103011608872
 #this sets it to 'hello', use the script linked to set it to your own
-#sudo echo 7F513D02E4A8C5E2FF1C39567390ADCA > /Library/Preferences/com.apple.VNCSettings.txt
+echo 7F513D02E4A8C5E2FF1C39567390ADCA | sudo tee /Library/Preferences/com.apple.VNCSettings.txt
 
 #Start VNC/reset changes
 sudo /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -restart -agent -console
@@ -30,9 +28,6 @@ brew cask install ngrok
 echo
 echo hey! we\'re almost there!
 echo you will need to do a couple things from SSH...
-echo sudo echo 7F513D02E4A8C5E2FF1C39567390ADCA \> /Library/Preferences/com.apple.VNCSettings.txt
-echo sudo /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -restart -agent -console
-echo sudo /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -activate
 echo ngrok authtoken YOUR_AUTHTOKEN_HERE
 echo ngrok tcp 5900
 echo
