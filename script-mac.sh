52d3b3bb315dc7a06662cbd1794aea6c2587bed8
kotaKat <35742614+dakotaKat@users.noreply.github.com>
2020-09-13 16:50:25 -0400

    Update script-mac.sh

--- script-mac.sh
+++ script-mac.sh
@@ -1,6 +1,5 @@
 # disable spotlight indexing
 sudo mdutil -i off -a
-echo Edit script-mac.sh in your fastmac repo to auto-run commands in your Mac instances
 
 #Create new account
 sudo dscl . -create /Users/vncuser UserShell /bin/bash
