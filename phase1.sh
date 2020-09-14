6d74d9efb990b2a2a427d325dee11f4a0cf93376
kotaKat <35742614+dakotaKat@users.noreply.github.com>
2020-09-13 20:33:09 -0400

    phase1: start creating account

---
+++ phase1.sh
@@ -0,0 +1,13 @@
+# disable spotlight indexing
+sudo mdutil -i off -a
+
+#install ngrok
+brew cask install ngrok
+
+#Create new account
+sudo dscl . -create /Users/vncuser
+sudo dscl . -create /Users/vncuser UserShell /bin/bash
+sudo dscl . -create /Users/vncuser RealName "VNC User"
+sudo dscl . -create /Users/vncuser UniqueID 1001
+sudo dscl . -create /Users/vncuser PrimaryGroupID 80
+sudo dscl . -create /Users/vncuser NFSHomeDirectory /Users/vncuser
